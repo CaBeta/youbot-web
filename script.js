29d72a83ff0884e8cb0bab45caa39ece0b43552f
carpon <carpon39038@163.com>
2017-03-31 15:34:57 +0800

    control v1.0

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 
   ros.on('connection', function() {
     console.log('Connected to websocket server.');
+    document.getElementById('message').innerHTML = 'Connected to websocket server.'
   });
 
   ros.on('error', function(error) {
@@ -26,33 +27,79 @@
     messageType : 'geometry_msgs/Twist'
   });
 
-  var twist = new ROSLIB.Message({
+  var up = new ROSLIB.Message({
     linear : {
-      x : 0.1,
-      y : 0.2,
-      z : 0.3
+      x : 0.4,
+      y : 0.0,
+      z : 0.0
     },
     angular : {
-      x : -0.1,
-      y : -0.2,
-      z : -0.3
+      x : -0.0,
+      y : -0.0,
+      z : -0.0
     }
   });
-  cmdVel.publish(twist);
-
+  var down = new ROSLIB.Message({
+    linear : {
+      x : -0.4,
+      y : 0.0,
+      z : 0.0
+    },
+    angular : {
+      x : -0.0,
+      y : -0.0,
+      z : -0.0
+    }
+  });
+  var left = new ROSLIB.Message({
+    linear : {
+      x : 0.0,
+      y : 0.0,
+      z : 0.0
+    },
+    angular : {
+      x : -0.0,
+      y : -0.0,
+      z :  0.2
+    }
+  });
+  var right = new ROSLIB.Message({
+    linear : {
+      x : 0.0,
+      y : 0.0,
+      z : 0.0
+    },
+    angular : {
+      x : -0.0,
+      y : -0.0,
+      z : -0.2
+    }
+  });
+  var stop = new ROSLIB.Message({
+    linear : {
+      x : 0.0,
+      y : 0.0,
+      z : 0.0
+    },
+    angular : {
+      x : -0.0,
+      y : -0.0,
+      z : -0.0
+    }
+  });
   // Subscribing to a Topic
   // ----------------------
 
-  var listener = new ROSLIB.Topic({
-    ros : ros,
-    name : '/listener',
-    messageType : 'std_msgs/String'
-  });
-
-  listener.subscribe(function(message) {
-    console.log('Received message on ' + listener.name + ': ' + message.data);
-    listener.unsubscribe();
-  });
+  // var listener = new ROSLIB.Topic({
+  //   ros : ros,
+  //   name : '/listener',
+  //   messageType : 'std_msgs/String'
+  // });
+  //
+  // listener.subscribe(function(message) {
+  //   console.log('Received message on ' + listener.name + ': ' + message.data);
+  //   listener.unsubscribe();
+  // });
 
   // Calling a service
   // -----------------
@@ -92,5 +139,26 @@
   //   console.log('MAX VAL: ' + value);
   // });
   window.onload=function(){
-    document.getElementById('')
+    var button_up = document.getElementById('up');
+    button_up.addEventListener('click',function () {
+      cmdVel.publish(up);
+    })
+    // up.addEventListener('mouseup',function () {
+    //   cmdVel.publish(stop);
+    // })
+
+    var button_down = document.getElementById('down');
+    button_down.addEventListener('click',function () {
+      cmdVel.publish(down);
+    })
+
+    var button_left = document.getElementById('left');
+    button_left.addEventListener('click',function () {
+      cmdVel.publish(left);
+    })
+
+    var button_right = document.getElementById('right');
+    button_right.addEventListener('click',function () {
+      cmdVel.publish(right);
+    })
   }
